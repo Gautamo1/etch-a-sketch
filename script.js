3b87986da2ef483b51e38f0f02b0fb1b9001da08
Gautamo1 <kumargautam802108@gmail.com>
2025-03-02 10:01:06 +0000

    dynamic grid size and hover effect

--- script.js
+++ script.js
@@ -20,18 +20,33 @@ container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
 container.style.boxSizing = 'border-box';
 
-const gridSize = 16;
-const squareSize = 960 / gridSize;
+function createGrid(gridSize){
+    container.innerHTML = '';
+    const squareSize = 960 / gridSize;
 
-for(let i = 0; i<gridSize*gridSize;i++){
-    const square = document.createElement('div');
-    square.style.width = `${squareSize}px`;
-    square.style.height = `${squareSize}px`;
-    square.style.boxSizing = 'border-box'; 
+    for(let i=0;i < gridSize*gridSize;i++){
+        const square = document.createElement('div');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        square.style.boxSizing = 'border-box';
 
     square.addEventListener('mouseover', () =>{
         square.style.backgroundColor = 'black';
     });
 
     container.appendChild(square);
+
+    }
 }
+
+createGrid(16);
+
+const button = document.createElement('button');
+button.textContent = 'Change Grid Size';
+document.body.insertBefore(button,container);
+
+button.addEventListener('click', () => {
+    let newSize = parseInt(prompt('Enter new grid size (max 100): '));
+    if(newSize > 100) newSize = 100;
+    if(newSize && newSize > 0) createGrid(newSize);
+})
